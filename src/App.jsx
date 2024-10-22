665b0fdeb74f25c907531cb2a98c64aab78bcff1
Prabhu <prabhuprathamesh444@gmail.com>
2024-10-22 16:02:19 +0530

    counter using react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './index.css'
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className='counter_box'>
+      <h1>counter {count}</h1>
+      <div className='counter_btn'>
+      <button className="increment" onClick={() => setCount(count + 1)}>increment</button>
+      <button className="reset" onClick={() => setCount(0)}>reset</button>
+      <button className="decrement" onClick={() => setCount(count - 1)}>decrement</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
