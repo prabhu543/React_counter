7f1d767367ac6322accd5515278e0ee390bb980e
Prabhu <prabhuprathamesh444@gmail.com>
2024-10-25 22:18:19 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import './index.css'
 const App = () => {
   const [count, setCount] = useState(0);
   return (
-    <div className='counter_box'>
+    <div className="main">
+      <div className='counter_box'>
       <h1>counter {count}</h1>
       <div className='counter_btn'>
       <button className="increment" onClick={() => setCount(count + 1)}>increment</button>
       <button className="reset" onClick={() => setCount(0)}>reset</button>
       <button className="decrement" onClick={() => setCount(count - 1)}>decrement</button>
       </div>
+    </div>
     </div>
   )
 }
